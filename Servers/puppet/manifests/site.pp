78013d06d1899c51a0bbd1206f738cb75a75f01d
Ian Garcez <ian@onespace.com.br>
2015-12-18 14:43:09 -0300

    add .gitignore and servers

---
+++ Servers/puppet/manifests/site.pp
@@ -0,0 +1,24 @@
+# create a new run stage to ensure certain modules are included first
+stage { 'pre':
+  before => Stage['main']
+}
+
+# add the baseconfig module to the new 'pre' run stage
+class { 'baseconfig':
+  stage => 'pre'
+}
+
+# set defaults for file ownership/permissions
+File {
+  owner => 'root',
+  group => 'root',
+  mode  => '0644',
+}
+
+# all boxes get the base config
+include baseconfig
+
+
+node 'static1', 'static2', 'static3', 'static4' {
+  include apache, apache_vhosts, php
+}
