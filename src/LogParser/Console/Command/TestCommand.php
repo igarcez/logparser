f8c9bdd8e77b08acaab7262177cb3bd62ee840a8
Ian Garcez <ian@onespace.com.br>
2015-12-18 15:31:11 -0300

    test command running

--- src/LogParser/Console/Command/TestCommand.php
+++ src/LogParser/Console/Command/TestCommand.php
@@ -3,9 +3,22 @@
  * @Author: Ian Garcez <ian@onespace.com.br>
  * @Date:   2015-12-18 15:15:53
  * @Last Modified by:   Ian Garcez
- * @Last Modified time: 2015-12-18 15:17:24
+ * @Last Modified time: 2015-12-18 15:30:50
  */
 namespace LogParser\Console\Command;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Formatter\OutputFormatterStyle;
 class TestCommand extends Command {
+  protected function configure() {
+    $this->setName("test")
+         ->setDescription("test command");
+  }
 
+  protected function execute(InputInterface $input, OutputInterface $output) {
+    $output->writeln('teste executado com sucesso');
+  }
 }
--- src/LogParser/Console/LogParserApplication.php
+++ src/LogParser/Console/LogParserApplication.php
@@ -3,12 +3,18 @@
  * @Author: Ian Garcez <ian@onespace.com.br>
  * @Date:   2015-12-18 15:14:33
  * @Last Modified by:   Ian Garcez
- * @Last Modified time: 2015-12-18 15:18:19
+ * @Last Modified time: 2015-12-18 15:23:37
  */
 namespace LogParser\Console;
 use Symfony\Component\Console\Application;
 use LogParser\Console\Command;
 
 class LogParserApplication extends Application {
+  public function __construct() {
+    parent::__construct('LogParser para Chaordic, por Ian Garcez');
 
+    $this->addCommands(array(
+        new Command\TestCommand()
+      ));
+  }
 }
