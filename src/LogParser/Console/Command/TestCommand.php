1d42ec2527ce998b3ed80b2606da771417b5da21
Ian Garcez <ian@onespace.com.br>
2015-12-18 16:33:04 -0300

    test server class

--- src/LogParser/Console/Command/TestCommand.php
+++ src/LogParser/Console/Command/TestCommand.php
@@ -3,7 +3,7 @@
  * @Author: Ian Garcez <ian@onespace.com.br>
  * @Date:   2015-12-18 15:15:53
  * @Last Modified by:   Ian Garcez
- * @Last Modified time: 2015-12-18 15:30:50
+ * @Last Modified time: 2015-12-18 16:32:38
  */
 namespace LogParser\Console\Command;
 use Symfony\Component\Console\Command\Command;
@@ -12,6 +12,7 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Formatter\OutputFormatterStyle;
+use LogParser\Server\Server;
 class TestCommand extends Command {
   protected function configure() {
     $this->setName("test")
@@ -19,6 +20,7 @@ class TestCommand extends Command {
   }
 
   protected function execute(InputInterface $input, OutputInterface $output) {
-    $output->writeln('teste executado com sucesso');
+    $server = new Server('192.168.0.42', 'vagrant', '/Users/ian/Development/Chaordic/LogParser/Servers/.vagrant/machines/static1/virtualbox/private_key');
+    $output->writeln($server->execCommand('ls'));
   }
 }
