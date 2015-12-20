3c0df9fbfd44b49c5e229eea6752610b1fb10de4
Ian Garcez <ian@onespace.com.br>
2015-12-20 15:03:55 -0300

    make test command unharmful

--- src/LogParser/Console/Command/TestCommand.php
+++ src/LogParser/Console/Command/TestCommand.php
@@ -3,7 +3,7 @@
  * @Author: Ian Garcez <ian@onespace.com.br>
  * @Date:   2015-12-18 15:15:53
  * @Last Modified by:   Ian Garcez
- * @Last Modified time: 2015-12-19 14:09:27
+ * @Last Modified time: 2015-12-20 15:03:38
  */
 namespace LogParser\Console\Command;
 use Symfony\Component\Console\Command\Command;
@@ -13,7 +13,6 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Formatter\OutputFormatterStyle;
 use LogParser\Server\Server;
-use LogParser\User;
 class TestCommand extends AbstractCommand {
   protected function configure() {
     $this->setName("test")
@@ -21,13 +20,6 @@ class TestCommand extends AbstractCommand {
   }
 
   protected function execute(InputInterface $input, OutputInterface $output) {
-    $um = new User\UserManager();
-    $um->addUser("ahucahucah-hauhecha-ehuacheuh", '192.168.0.42"');
-    $um->writeUsers();
-    // foreach ($this->servers as $server) {
-    //   $server = new Server($server->host, $server->user, $server->key_file, $server->log_path);
-    //   $path = $server->getLogPath();
-    //   $output->writeln($server->execCommand("cat $path/access.log"));
-    // }
+    $output->writeln('hm?');
   }
 }
